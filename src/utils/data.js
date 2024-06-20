9f8d85a7c721f38b1a6e2fa431db49ee4a8b2b22
Ilyaslat <64992874+Ilyaslat@users.noreply.github.com>
2024-06-20 22:57:45 +0200

    Add files via upload

---
+++ src/utils/data.js
@@ -0,0 +1,71 @@
+export const SKILLS = [
+  {
+    title: "Frontend",
+    icon: "./assets/images/front.png",
+    skills: [
+      { skill: "HTML5", percentage: "80%" },
+      { skill: "CSS3 (Sass/SCSS)", percentage: "90%" },
+      { skill: "JavaScript", percentage: "80%" },
+      { skill: "React.js", percentage: "84%" },
+    ],
+  },
+  {
+    title: "Backend",
+    icon: "./assets/images/back.png",
+    skills: [
+      { skill: "Node.js", percentage: "73%" },
+      { skill: "PHP", percentage: "70%" },
+      { skill: "Laravel", percentage: "70%" },
+    ],
+  },
+  {
+    title: "Outils",
+    icon: "./assets/images/tools.png",
+    skills: [
+      { skill: "GitHub", percentage: "85%" },
+      { skill: "Visual Studio Code", percentage: "88%" },
+      { skill: "Responsive Design", percentage: "80%" },
+    ],
+  },
+  {
+    title: "Soft Skills",
+    icon: "./assets/images/skills.png",
+    skills: [
+      { skill: "Organisation et gestion", percentage: "85%" },
+      { skill: "Communication verbale et écrite", percentage: "85%" },
+      { skill: "Travail d'équipe", percentage: "88%" },
+      { skill: "Travail en autonomie", percentage: "85%" },
+    ],
+  },
+];
+
+export const WORK_EXPERINCE = [
+  {
+    title: "Formation Développeur Web et Web mobile",
+    date: "Septembre 2023 - Avril 2024",
+    responsabilites: [
+      "J'ai récemment terminé une formation en développement web et web mobile chez M2I, ça été une expérience très enrichissante. J'y ai appris à créer des sites et des applications web modernes et interactifs en utilisant des technologies comme HTML, CSS, JavaScript, PHP ainsi que des frameworks tels que React et Laravel. J'ai aussi découvert l'importance du design responsive et de l'optimisation des performances pour offrir une expérience utilisateur fluide sur tous les types d'appareils. Grâce à cette formation, j'ai acquis non seulement des compétences techniques solides, mais aussi une approche créative et organisée pour aborder les exigences du développement web.",
+    ],
+  },
+  {
+    title: "Stage chez ADEO",
+    date: "Avril 2024 - Mai 2024",
+    responsabilites: [
+      "Grâce à mon stage chez ADEO, j'ai eu l'opportunité de découvrir le monde de l'entreprise et de comprendre le véritable métier de développeur web. En participant activement à divers projets, j'ai pu développer des compétences essentielles telles que la programmation, la gestion de bases de données et le travail en équipe. Les tâches qui m'ont été confiées m'ont permis d'acquérir une expérience pratique précieuse et de mieux appréhender les défis et les exigences du secteur du développement web.",
+    ],
+  },
+  {
+    title: "Formation Udémy - Developpeur web et web mobile",
+    date: "Mars 2023 ",
+    responsabilites: [
+      "Aprés l'achat d'une formation de développeur web sur la plateforme Udemy, cela a véritablement transformé ma passion pour le développement web. Grâce à des cours bien structurés et des projets pratiques, j'ai pu approfondir mes connaissances et acquérir de nouvelles compétences. Cette formation m'a permis de renforcer ma compréhension des langages de programmation et des outils de développement.",
+    ],
+  },
+  {
+    title: "Nursery Numérique",
+    date: "Décembre 2021 - Janvier 2022",
+    responsabilites: [
+      "Pendant mon court stage à la Nursery Numérique, j'ai découvert les bases du développement, notamment HTML et CSS j'ai également appris la méthode de travail SCRUM et maîtrisé l'utilisation de GitHub, puis en équipe, nous avons développé un projet fil rouge, ce qui m'a permis de mettre en pratique ces nouvelles compétences de manière concrète et collaborative.",
+    ],
+  },
+];
